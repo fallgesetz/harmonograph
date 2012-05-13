487dfa007acf01ab7e6bf3def19fc45f64f55662
Leah Xue <fallgesetz@gmail.com>
2012-05-12 19:12:59 -0700

    bug fix

--- static/main.js
+++ static/main.js
@@ -2,6 +2,7 @@ window.onload = function () {
     var canvas = document.getElementById("harmonograph");
     var context = canvas.getContext("2d");
     var STEP_SIZE = 0.02;
+    var eps = 0.1;
 
     function draw_path(ctx, path)
     {
@@ -18,7 +19,7 @@ window.onload = function () {
             y = coord[1];
             ctx.lineTo(x,y);
         }
-        ctx.closePath();
+//        ctx.closePath();
         ctx.stroke();
         ctx.restore();
     }
@@ -26,7 +27,7 @@ window.onload = function () {
     function tween_path(ctx, path, fps, total) {
     	var seconds_delay = Math.round(1000/fps);
         console.log(seconds_delay);
-    	for(var i = 0; i < total; i += seconds_delay) {
+    	for(var i = 0; i <= total; i += seconds_delay) {
             var begin = Math.round(i/total * path.length);
             var cur_path = path.slice(0, begin);
             window.setTimeout(function(cur_path, ctx) { 
@@ -64,14 +65,17 @@ window.onload = function () {
     function input_polar_curve_to_path(x_str, y_str)
     {
 	var points = [];
-	for (var t = 0; t < 2*Math.PI; t += step_size)
+	for (var t = 0; t <= 2*Math.PI + eps; t += STEP_SIZE)
 	{
-		x = eval(x_str);
-		y = eval(y_str);
+		x = eval(x_str) + context.canvas.width/2;
+		y = eval(y_str) + context.canvas.height/2;
+                points.push([x,y]);
 	}
+        console.log(points);
 	return points;
     }
 
-    l_path = lissajous_curve(200, 200, 150, 50, 0.5, 0.5, 300, 300, 1);
+//    l_path = lissajous_curve(200, 200, 150, 50, 0.5, 0.5, 300, 300, 1);
+    l_path = input_polar_curve_to_path("100*Math.cos(t)", "100* Math.sin(t)");
     tween_path(context, l_path, 24, 12000);
 };
